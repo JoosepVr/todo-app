1aed9edfd6e81f73b432c2a36dfcd66b3e9925b5
Joosep <joosep.vorno@gmail.com>
2023-04-13 21:40:06 +0300

    Everything in 1 line

--- gui.py
+++ gui.py
@@ -5,6 +5,6 @@ label = Pys.Text("Type in a to-do")
 input_box = Pys.InputText(tooltip="Enter todo")
 add_button = Pys.Button("Add")
 
-window = Pys.Window("My To-Do App", layout=[[label], [input_box, add_button]])
+window = Pys.Window("My To-Do App", layout=[[label, input_box, add_button]])
 window.read()
 window.close()
