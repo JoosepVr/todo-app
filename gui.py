cd5ed7393e070e07532dbe237cc3385efb682df1
Joosep <joosep.vorno@gmail.com>
2023-04-18 21:06:48 +0300

    added Complete, and Exit buttons

--- gui.py
+++ gui.py
@@ -7,9 +7,14 @@ add_button = Pys.Button("Add")
 list_box = Pys.Listbox(values=functions.get_todos(), key="todos",
 					   enable_events=True, size=[45,10])
 edit_button = Pys.Button("Edit")
+complete_button = Pys.Button("Complete")
+exit_button = Pys.Button("Exit")
 
 window = Pys.Window("My To-Do App",
-					layout=[[label], [input_box, add_button], [list_box, edit_button]],
+					layout=[[label],
+							[input_box, add_button],
+							[list_box, edit_button, complete_button],
+							[exit_button]],
 					font=("Helvetica", 20))
 
 while True:
@@ -23,6 +28,7 @@ while True:
 		todos.append(new_todo)
 		functions.write_todos(todos)
 		window["todos"].update(values=todos)
+
 	elif event == "Edit":
 		todo_to_edit = values["todos"][0]
 		new_todo = values["todo"]
@@ -32,9 +38,19 @@ while True:
 		todos[index] = new_todo
 		functions.write_todos(todos)
 		window["todos"].update(values=todos)
+
+	elif event == "Complete":
+		todo_to_complete = values["todos"][0]
+		todos = functions.get_todos()
+		todos.remove(todo_to_complete)
+		functions.write_todos(todos)
+		window["todos"].update(values=todos)
+		window["todo"].update(values="")
+
+	elif event == Pys.WIN_CLOSED or event == 'Exit':
+		break
+
 	elif event == "todos":
 		window["todo"].update(value=values["todos"][0])
-	elif event == Pys.WIN_CLOSED:
-		break
 
 window.close()
